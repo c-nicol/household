e54470489ae9add5d411c04e19e5f3bb609a25c9
c-nicol <christopher.nicol@hotmail.com>
2024-02-16 02:07:16 +1100

    Persist chores in local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,10 @@
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { ChoresSlice, createChoresSlice } from './choresSlice';
 import { create } from 'zustand'
 
 export const useStore = create<ChoresSlice>((...a) => ({
-  ...createChoresSlice(...a)
+  ...persist(createChoresSlice, {
+    name: "chores-storage",
+    storage: createJSONStorage(() => localStorage)
+  })(...a)
 }))
