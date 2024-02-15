4e146507979b0de9118d74705ea836d77e58a3d0
c-nicol <christopher.nicol@hotmail.com>
2024-02-15 23:47:47 +1100

    Scaffold Chores Sidebar & Board

---
+++ src/features/chores/chores-board.tsx
@@ -0,0 +1,3 @@
+export function ChoresBoard() {
+  return <div className="h-full rounded-xl border border-dashed p-4">Chores Board Placeholder</div>;
+}
--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,3 +1,5 @@
+import { ChoresBoard } from '@/features/chores/chores-board';
+import { ChoresSidebar } from '@/features/chores/chores-sidebar';
 import { createLazyFileRoute } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/')({
@@ -6,8 +8,22 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div className="mx-auto max-w-6xl px-4">
+      <div className="bg-white">
+        <div>
+          <main>
+            <section className="pb-24 pt-6">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+                <ChoresSidebar />
+
+                <div className="lg:col-span-3">
+                  <ChoresBoard />
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
