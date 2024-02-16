5c45af4b81afde2a4eed056ff44eed3386c56b14
c-nicol <christopher.nicol@hotmail.com>
2024-02-16 19:27:56 +1100

    Configure area filters in store

--- src/store/choresSlice.tsx
+++ src/store/choresSlice.tsx
@@ -2,6 +2,7 @@ import { StateCreator } from 'zustand';
 
 export type Chore = {
   id: string;
+  createdAt: Date;
   title: string;
   area: string;
   duration: number;
@@ -15,6 +16,8 @@ export interface ChoresSlice {
   /* Filters */
   durationRange: number[];
   setDurationRange: (range: number[]) => void;
+  areaFilters: string[];
+  toggleAreaFilter: (areaFilter: string) => void;
 }
 
 export const createChoresSlice: StateCreator<ChoresSlice, [], [], ChoresSlice> = (set) => ({
@@ -24,4 +27,11 @@ export const createChoresSlice: StateCreator<ChoresSlice, [], [], ChoresSlice> =
     set((state) => ({ chores: state.chores.filter((chore) => chore.id !== choreId) })),
   durationRange: [0, 60],
   setDurationRange: (range) => set({ durationRange: range }),
+  areaFilters: ['bathroom', 'bedroom', 'kitchen', 'laundry'],
+  toggleAreaFilter: (areaFilter) =>
+    set((state) => ({
+      areaFilters: state.areaFilters.includes(areaFilter)
+        ? state.areaFilters.filter((af) => af !== areaFilter)
+        : [...state.areaFilters, areaFilter],
+    })),
 });
