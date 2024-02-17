adfd39b57f6247e94f333d09618047b74e127284
c-nicol <christopher.nicol@hotmail.com>
2024-02-18 00:07:40 +1100

    Remove sort button

--- src/features/chores/sidebar/chores-sidebar.tsx
+++ src/features/chores/sidebar/chores-sidebar.tsx
@@ -1,17 +1,10 @@
-import { Button } from '@/components/button';
-import { IconSortDescending } from '@tabler/icons-react';
 import { ChoresFilters } from './chores-filters';
 import { CreateChore } from './create-chore';
 
 export function ChoresSidebar() {
   return (
     <div className="sticky top-[160px] flex flex-col gap-y-4">
-      <div className="flex justify-between gap-x-2">
-        <CreateChore />
-        <Button variant="link" className="p-2.5">
-          <IconSortDescending className="h-5 w-5" />
-        </Button>
-      </div>
+      <CreateChore />
       <ChoresFilters />
     </div>
   );
