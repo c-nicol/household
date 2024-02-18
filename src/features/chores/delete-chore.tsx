7e93be0e5c7330939374a503de816b404a9b21c3
c-nicol <christopher.nicol@hotmail.com>
2024-02-18 11:13:30 +1100

    Add confirmation modal when deleting chore

--- src/features/chores/delete-chore.tsx
+++ src/features/chores/delete-chore.tsx
@@ -1,6 +1,18 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/alert-dialog';
 import { Button } from '@/components/button';
 import { useStore } from '@/store/store';
 import { IconTrash } from '@tabler/icons-react';
+import { toast } from 'sonner';
 
 type Props = {
   choreId: string;
@@ -9,15 +21,28 @@ type Props = {
 export function DeleteChore({ choreId }: Props) {
   const deleteChore = useStore.getState().removeChore;
 
+  const handleDeleteChore = () => {
+    deleteChore(choreId);
+    toast.success('Chore has been deleted');
+  };
+
   return (
-    <Button
-      className="p-3"
-      variant="destructive"
-      onClick={() => {
-        deleteChore(choreId);
-      }}
-    >
-      <IconTrash className="h-4 w-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="p-3" variant="destructive">
+          <IconTrash className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
+          <AlertDialogDescription>This action cannot be undone.</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteChore}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
