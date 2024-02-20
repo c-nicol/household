9f6cf7437f3c7576262d6c23bd7b254b9cfb088f
c-nicol <christopher.nicol@hotmail.com>
2024-02-20 23:42:34 +1100

    Update Mobile filters

--- src/features/chores/sidebar/chores-sidebar.tsx
+++ src/features/chores/sidebar/chores-sidebar.tsx
@@ -1,15 +1,63 @@
+import { Button } from '@/components/button';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/drawer';
+import { IconFilter } from '@tabler/icons-react';
 import { AreaFilter } from './area-filter';
 import { CreateChore } from './create-chore';
 import { DurationFilter } from './duration-filter';
 
 export function ChoresSidebar() {
   return (
-    <div className="sticky top-[160px] flex flex-col gap-y-4">
+    <div className="sticky top-[160px] flex gap-x-2 gap-y-4 lg:flex-col">
       <CreateChore />
-      <div className="flex flex-col gap-y-6">
-        <AreaFilter />
-        <DurationFilter />
-      </div>
+      <DesktopFilters />
+      <MobileFilters />
     </div>
+  );
+}
+
+function DesktopFilters() {
+  return (
+    <div className="hidden flex-col gap-y-6 lg:flex">
+      <AreaFilter />
+      <DurationFilter />
+    </div>
+  );
+}
+
+function MobileFilters() {
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <Button variant="outline" className="p-3 lg:hidden">
+          <IconFilter className="h-4 w-4" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Select Filters</DrawerTitle>
+          <DrawerDescription>Choose by chore area & duration</DrawerDescription>
+        </DrawerHeader>
+
+        <div className="flex flex-col gap-y-6 p-6">
+          <AreaFilter />
+          <DurationFilter />
+        </div>
+
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">Close</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
